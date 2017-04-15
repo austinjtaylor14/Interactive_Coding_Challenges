7e58a08ecb70a0d2beadb7c5ba8020ab2bddf0f1
Austin <austinjtaylor14@gmail.com>
2017-04-15 15:51:18 -0400

    Formatting

--- find_diff_in_strings.py
+++ find_diff_in_strings.py
@@ -8,7 +8,7 @@ Created on Sat Apr 15 15:31:34 2017
 
 class Solution(object):
 
-    def find_diff(s, t):
+    def find_diff(self, s, t):
         for i in range(max(len(s), len(t))):
             try:
                 if sorted(s)[i] != sorted(t)[i]:
